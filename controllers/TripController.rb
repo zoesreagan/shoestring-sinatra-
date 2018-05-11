fe4f38f014aae80d67e60770c74edb7466e87c33
Hannah Werman <hannah.werman@gmail.com>
2018-05-11 14:06:08 -0500

    created delete route in trip controller

---
+++ controllers/TripController.rb
@@ -0,0 +1,33 @@
+class TripController < ApplicationController
+	# get all of the trips that belong to a user
+	# this might be better in the user controller? 
+	# get '/' do
+	# 	# find the user by id
+	# 	@trips = Trip.all
+	# end
+
+	#check to make sure these line up with database and form before pushing
+	# post '/' do
+	# 	payload_body = request.body.read
+	# 	payload = JSON.parse(payload_body).symbolize_keys
+
+	# 	@trip = Trip.new
+	# 	@trip.name = payload[:name]
+	# 	@trip.budget = payload[:budget]
+
+
+
+	# 	@trip.save
+	# end
+
+	delete '/:id' do
+		@trip = Trip.find params[:id]
+		@trip.destroy
+
+		{
+			success: true,
+			message: "trip #{@trip.name} deleted successfully"
+		}.to_json
+	end
+
+end
