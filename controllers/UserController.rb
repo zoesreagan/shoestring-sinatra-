8634c6498d2de7a34c72a88b6f839ccdc91a3239
Zoe <zoereagan@gmail.com>
2018-05-14 09:30:54 -0500

    added user seed data

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -6,14 +6,40 @@ class UserController < ApplicationController
       @users.to_json
     end
 
-##add route
-  # get '/add' do
+##create user route
+  post '/' do
+    payload_body = request.body.read
+    payload = JSON.parse(payload_body).symbolize_keys
+
+    @user = User.new
+    @user.name = payload[:name]
+    @user.photo = payload[:name]
+    @user.save
+    @user.to_json
+    ##username
+    ##password
+
+  end
+
+##update user route
+
+  # put ':/id' do
+  #   payload_body = request.body.read
+  #   @payload = JSON.parse(payload_body).symbolize_keys
   #
-  #   @user = User.new
-  #   @user.name =
-  #   ##username
-  #   ##password
+  #   @user = User.find params[:id]
+  #   @user.name = payload[:name]
+  #   #username
+  #   #password
+  #   #picture
+  #   @user.save
+  #   {
+  #     success: true,
+  #     message: "You updated item \##{@waiter.id}",
+  #     updated_user: @user
+  #   }.to_json
   # end
+
 
 
 
