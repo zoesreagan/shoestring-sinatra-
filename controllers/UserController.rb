ebad5e498d990c93d3375b57651a387f1c4a14ff
Samat Baltin <somych1@gmail.com>
2018-05-14 09:45:06 -0500

    added login route to user controller

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -16,6 +16,7 @@ class UserController < ApplicationController
 	  	user.save
 
 	  	session[:logged_in] = true
+	  	session[:user_name] = user.name
 	  	session[:username] = user.username
 	  	session[:user_id] = user.id
 
@@ -35,5 +36,42 @@ class UserController < ApplicationController
 	  		message: 'you are logged in and you have a cookie attached to all the responses'
 	  	}.to_json
 	end
+
+	post '/login' do
+	  	puts ''
+	  	puts "login. hitting session"
+	  	pp session
+	  	puts ''
+
+
+	  	username = @payload[:username]
+	  	password = @payload[:password]
+
+	  	user = User.find_by username: username
+
+	  	if user && user.authenticate(password)
+	  		session[:logged_in] = true
+	  		session[:name] = user.name
+	  		session[:username] = username
+	  		session[:user_id] = user.id
+
+	  		puts ''
+		  	puts "here is session in login after logging in"
+		  	pp session
+		  	puts ''
+		  	{
+		  		succses: true,
+		  		user_name: user.name,
+		  		user_id: user.id,
+		  		username: username,
+		  		message: 'Login succsesful'
+		  	}.to_json
+	  	else
+	  		{
+	  			succses: false,
+	  			message: 'Invalid Username or pw'
+	  		}.to_json
+	  	end
+  	end
 
 end
